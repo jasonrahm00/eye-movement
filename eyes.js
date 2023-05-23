a195a247eef943534093025b95e6bf22db96e260
Jason Rahm <jasonrahm@gmail.com>
2023-05-23 16:52:39 -0600

    functions added to trigger and reset eye follow

--- eyes.js
+++ eyes.js
@@ -1,17 +1,26 @@
 const balls = document.getElementsByClassName('ball')
 
-document.onmousemove = (event) => {
-  const x = (event.clientX * 100) / window.innerWidth + '%'
-  const y = (event.clientY * 100) / window.innerHeight + '%'
+// Add event listener to follow cursor
+function follow() {
+  document.addEventListener('mousemove', (event) => {
+    const x = (event.clientX * 100) / window.innerWidth + '%'
+    const y = (event.clientY * 100) / window.innerHeight + '%'
 
-  for (let i = 0; i < balls.length; i++) {
-    const ball = balls[i]
+    for (let i = 0; i < balls.length; i++) {
+      const ball = balls[i]
 
-    ball.style.left = x
-    ball.style.top = y
-    ball.transform = 'translate(-' + x + ',-' + y + ')'
-    ball.style.left = x
-    ball.style.top = y
-    ball.transform = 'translate(-' + x + ',-' + y + ')'
-  }
+      ball.style.left = x
+      ball.style.top = y
+      ball.transform = 'translate(-' + x + ',-' + y + ')'
+      ball.style.left = x
+      ball.style.top = y
+      ball.transform = 'translate(-' + x + ',-' + y + ')'
+    }
+  })
+}
+
+// Reload page to reset
+// TODO: find another way to reset without reloading page
+function stopFollow() {
+  location.reload()
 }
